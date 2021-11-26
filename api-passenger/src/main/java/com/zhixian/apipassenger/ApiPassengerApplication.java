42df237053dddd6303a4697903e3eaf36c6fd4e1
AwesomeyouthZhang <zhang.zx.1073538693@gmail.com>
2021-11-26 23:53:44 +0800

    支持OpenFeign和Swagger2

--- api-passenger/src/main/java/com/zhixian/apipassenger/ApiPassengerApplication.java
+++ api-passenger/src/main/java/com/zhixian/apipassenger/ApiPassengerApplication.java
@@ -2,10 +2,13 @@ package com.zhixian.apipassenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-
+@EnableFeignClients
+@EnableSwagger2
 public class ApiPassengerApplication {
 
     public static void main(String[] args) {
